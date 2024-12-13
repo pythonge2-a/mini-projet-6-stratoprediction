763235e81ed7820f78360490670bf39cfb315bd1
Théo Welker <theo.welker@heig-vd.ch>
2024-12-13 10:06:32 +0100

    Récupération des données

--- strato_prediction/analyse/__init__.py
+++ strato_prediction/analyse/__init__.py
@@ -6,8 +6,17 @@ class AnalyserGRIB:
         self.level = None
         self.date = None
         self.time = None
+        self.temperature = None
+        self.pressure = None
+        self.altitude = None
 
     def analyser_grib(self, file_path):
+        #filtre zone coordonnées:
+        zlatmin = 46.4008333
+        zlatmax = 46.8072222
+        zlonmin = 6.211111111111111
+        zlonmax = 6.9991666666666665
+
         try:
             grbs = pygrib.open(file_path)
             for grb in grbs:
@@ -28,7 +37,13 @@ class AnalyserGRIB:
                 print(f"Date: {self.date}")
                 print(f"Time: {self.time}")
             print(dir(grb))
-            print(grb.data(lat1=45.20,lat2=45.3,lon1=8.2,lon2=8.3))
+            data, lats, lons = grb.data(lat1=zlatmin,lat2=zlatmax,lon1=zlonmin,lon2=zlonmax)
+            print(f"data:{data}")
+            print(f"lats:{lats}")
+            print(f"lons:{lons}")
+            print(f"dataDate:{self.date}")
+            print(f"dataTime:{self.time}")
+            print(f"datashape:{data.shape}")
         except Exception as e:
             print(f"Erreur lors de l'analyse du fichier GRIB : {e}")
         return "Données analysées"  
