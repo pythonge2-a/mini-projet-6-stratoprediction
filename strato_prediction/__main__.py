bd2e1534ea1b83f8535a45109e1cbbf5fd3ce3fc
Théo Welker <theo.welker@heig-vd.ch>
2024-12-13 08:35:51 +0100

    Avancée récupération des données

--- strato_prediction/__main__.py
+++ strato_prediction/__main__.py
@@ -1,14 +1,14 @@
 from strato_prediction import init_project
-from strato_prediction.analyse import analyser_grib
+from strato_prediction.analyse import AnalyserGRIB
 from strato_prediction.display import afficher_resultats
 
 def main():
     # Initialiser le projet
     init_project()
-
+    analyser = AnalyserGRIB()
     # Tester l'analyse et l'affichage des résultats
-    file_path = "assets/gfs.t00z.pgrb2.1p00 (1).f000"  # Utilisez un chemin de fichier de test
-    data = analyser_grib(file_path)
+    file_path = "assets/gfs.t06z.pgrb2.0p25.f000"  # Utilisez un chemin de fichier de test
+    data = analyser.analyser_grib(file_path)
 
     # Afficher les résultats
     afficher_resultats(data)
--- strato_prediction/analyse/__init__.py
+++ strato_prediction/analyse/__init__.py
@@ -1,11 +1,34 @@
 import pygrib
+class AnalyserGRIB:
+    def __init__(self):
+        self.uWind = None
+        self.vWind = None
+        self.level = None
+        self.date = None
+        self.time = None
 
-def analyser_grib(file_path):
-    try:
-        grbs = pygrib.open(file_path)
-        for grb in grbs:
-            print(grb)
-            print(f"description:{grb.name}\n")
-    except Exception as e:
-        print(f"Erreur lors de l'analyse du fichier GRIB : {e}")
-    return "Données analysées"  
+    def analyser_grib(self, file_path):
+        try:
+            grbs = pygrib.open(file_path)
+            for grb in grbs:
+                print(grb)
+                print(grb.name)
+                if "U component of wind" in grb.name:         
+                    self.uWind = 2          
+                    
+                if "V component of wind" in grb.name:
+                    self.vWind = 1 
+
+                self.level = grb.level         
+                self.date = grb.dataDate           
+                self.time = grb.dataTime
+                print(f"vWind: {self.vWind}")
+                print(f"uWind: {self.uWind}")
+                print(f"Level: {self.level}")
+                print(f"Date: {self.date}")
+                print(f"Time: {self.time}")
+            print(dir(grb))
+            print(grb.data(lat1=45.20,lat2=45.3,lon1=8.2,lon2=8.3))
+        except Exception as e:
+            print(f"Erreur lors de l'analyse du fichier GRIB : {e}")
+        return "Données analysées"  
