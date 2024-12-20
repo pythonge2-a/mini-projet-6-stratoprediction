bfd2d2353d813c05c80f4a97bfc2a3f894af73f6
Théo Welker <theo.welker@heig-vd.ch>
2024-12-21 00:28:11 +0100

    SubModule simulation finished

--- strato_prediction/GRIB/data_retrieval.py
+++ strato_prediction/GRIB/data_retrieval.py
@@ -6,17 +6,17 @@ def download_grib_file(url, output_path):
     pass
 
 def load_grib_data(file_path):
-    filter_keys = {'typeOfLevel': "isobaricInhPa"}
-    ds = xr.open_dataset(file_path, engine='cfgrib', filter_by_keys=filter_keys)
+    filter_keys = {"typeOfLevel": "isobaricInhPa"}
+    ds = xr.open_dataset(file_path, engine="cfgrib", filter_by_keys=filter_keys)
 
     # Structure contenant toutes les données grib nécessaires
     data = {
-        'pressure': ds.variables['isobaricInhPa'],
-        'latitude': ds.variables['latitude'],
-        'longitude': ds.variables['longitude'],
+        "pressure": ds.variables["isobaricInhPa"],
+        "latitude": ds.variables["latitude"],
+        "longitude": ds.variables["longitude"],
         
-        'u_wind': ds.variables['u'],
-        'v_wind': ds.variables['v'],
-        'temperature': ds.variables['t']
+        "u_wind": ds.variables["u"],
+        "v_wind": ds.variables["v"],
+        "temperature": ds.variables["t"]
     }
     return data
--- strato_prediction/simulation/simulation.py
+++ strato_prediction/simulation/simulation.py
@@ -1,18 +1,47 @@
+# import strato_prediction.simulation.conversions
+from .utils import Conversion
+
 class Balloon:
-    def __init__(self, lon_start, lat_start, pressure_start, ascendion_speed=5, time_step=60):
-        self.lon = lon_start
-        self.lat = lat_start
+    def __init__(self, start_lon, start_lat, pressure_start, ascendion_speed=5, time_step=60):
         self.pressure = pressure_start
+        self.lat = start_lat
+        self.lon = start_lon
+        self.pressure_alt0 = 1000
         self.z_speed = ascendion_speed
         self.u_speed_interp = None
         self.v_speed_interp = None
+        self.temperature_alt0_interp = None
         self.time_step = time_step
-        self.trajectory = None #list coord 3d (lon, lat, pres/alt)
+        self.trajectory = {
+            "altitudes" : [],
+            "latitudes" : [],
+            "longitudes" : []
+        }
 
     #calculations
-    def prepare_wind_interpolators(self, data):
-        pass
-    def get_wind_at_point(self, data):
-        pass
-    def get_next_point(self, data):
-        pass
+    def get_wind_at_point(self, interpolatorU, interpolatorV):
+        self.u_speed_interp = interpolatorU((self.pressure, self.lat, self.lon))
+        self.v_speed_interp = interpolatorV((self.pressure, self.lat, self.lon))
+
+    def get_temperature_at_point(self, temp_interp):
+        self.temperature_alt0_interp = temp_interp((self.pressure_alt0, self.lat, self.lon))
+    
+    def get_next_point(self):
+        conversions = Conversion()
+        
+        d_lat = self.u_speed_interp*self.time_step
+        d_lon = self.v_speed_interp*self.time_step
+        
+        altitude = conversions.pressure_to_altitude(self.pressure, self.temperature_alt0_interp)
+        d_lat_degrees = conversions.meters_to_latitude(d_lat)
+        d_lon_degrees = conversions.meters_to_longitude(d_lon)
+        
+        new_altitude = float(altitude + self.z_speed*self.time_step)
+        new_latitude = float(self.lat + d_lat_degrees)
+        new_longitude = float(self.lon+ d_lon_degrees)
+        
+        self.trajectory["altitudes"].append(new_altitude)
+        self.trajectory["latitudes"].append(new_latitude)
+        self.trajectory["longitudes"].append(new_longitude)
+        
+        
