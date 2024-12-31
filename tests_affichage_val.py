28ff050795a6b5343c59743a9afd2066b097ab68
Valentin Imhof <valentin.imhof@heig-vd.ch>
2024-12-31 16:28:43 +0100

    automatised

--- tests_affichage_val.py
+++ tests_affichage_val.py
@@ -13,17 +13,19 @@ ax.set_ylim3d(0, 5)
 ax.set_zlim3d(0, 5)
 
 # Données des points 
-points = ((0.8439, 2.5321, 4.2142), (3.532, 2.1024, 0.2314))
+points = ((0.8439, 2.5321, 4.2142), (3.532, 2.1024, 0.2314), (1.4, 4.6, 0.2), (1, 2, 3))
 
 # Ajout des deux points
-ax.scatter3D(points[0][0], points[0][1], points[0][2], color='red', label='point rouge')
-ax.scatter3D(points[1][0], points[1][1], points[1][2], color = 'blue', label='point bleu')
+for i in points:
+    ax.scatter3D(i[0], i[1], i[2], color='red', label='point rouge')
 
-# Ajout du segment entre les deux points
-x = [points[0][0], points[1][0]]  # Coordonnées x des deux points
-y = [points[0][1], points[1][1]]  # Coordonnées y des deux points
-z = [points[0][2], points[1][2]]  # Coordonnées z des deux points
-ax.plot3D(x, y, z, color='green', label='segment0')
+
+# Ajout du segment entre les points
+for i in range(len(points) - 1):  # Parcourt jusqu'à l'avant-dernier point
+    x = [points[i][0], points[i + 1][0]]
+    y = [points[i][1], points[i + 1][1]]
+    z = [points[i][2], points[i + 1][2]]
+    ax.plot3D(x, y, z, color='green', label=f'segment{i}')
 
 # Ajout d'un titre et d'une légende
 ax.set_title('Les petits poingues')
