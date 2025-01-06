f6949c6f574f69eaab57d9ddb0cadcf76fd7830b
Dany <dany.caputo@hes-so.ch>
2025-01-06 11:18:57 +0100

    Console test V3

--- console_test.py
+++ console_test.py
@@ -11,4 +11,56 @@ def is_float(string):
     except ValueError:
         return False
 
-
+while True:
+    result = radiolist_dialog(
+    title="Lancement simulateur",
+    text="Quelle type de simulation voulez-vous lancer?",
+    values=[
+        ("sim1", "Trajet simple"),
+        ("sim2", "Distance point d'attérissage minimisée"),
+        ("sim3", "Aucune, fin de simulation")
+    ]
+    ).run()
+    if result == "sim2":
+        print("Maximisation pas prête.")
+        break
+    elif result == "sim1":
+        lon_str = input_dialog(
+        title="Coordonnées initiales",
+        text="Entrez la longitude au format float.",
+        ).run()
+        print(f"'{lon_str}'")
+        while(not is_float(lon_str)) :
+            print(f"'{lon_str}'")
+            lon_str = input_dialog(
+            title="Coordonnées initiales",
+            text="Mauvaise input. Entrez la longitude au format float.",
+            ).run()
+        lat_str = input_dialog(
+        title="Coordonnées initiales",
+        text="Entrez la latitude au format float.",
+        ).run()
+        while(not is_float(lat_str)) :
+            lat_str = input_dialog(
+            title="Coordonnées initiales",
+            text="Mauvaise input. Entrez la latitude au format float.",
+            ).run()
+        pressure_str = input_dialog(
+        title="Coordonnées initiales",
+        text="Entrez la pression au format float.",
+        ).run()
+        print(f"'{pressure_str}'")
+        while(not is_float(pressure_str)) :
+            pressure_str = input_dialog(
+            title="Coordonnées initiales",
+            text="Mauvaise input. Entrez la pression au format float.",
+            ).run()
+        lon_start=float(lon_str)
+        lat_start=float(lat_str)
+        pressure_start=float(pressure_str)
+        message_dialog(
+            text="Simulation avec longitude: {lon_start}, latitude: {lat_start} et pression: {pressure_start}."
+        )
+        print("executer code ici")
+    else:
+        break
