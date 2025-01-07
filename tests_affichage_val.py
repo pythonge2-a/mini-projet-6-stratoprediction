9d384241dcadb1ff314e861944e77267571a1ae0
Valentin Imhof <valentin.imhof@heig-vd.ch>
2025-01-07 15:58:47 +0100

    Chais po

--- tests_affichage_val.py
+++ tests_affichage_val.py
@@ -7,34 +7,35 @@ from folium.plugins import AntPath, Fullscreen, GroupedLayerControl, MiniMap, Me
 import http.server
 import socketserver
 
-fig = plt.figure() 
+if(0):
+    fig = plt.figure() 
 
-# Définir l'axe 3D
-ax = plt.axes(projection='3d')
+    # Définir l'axe 3D
+    ax = plt.axes(projection='3d')
 
-# Définir les limites pour que les axes partent de 0
-ax.set_xlim3d(0, 5)
-ax.set_ylim3d(0, 5)
-ax.set_zlim3d(0, 5)
+    # Définir les limites pour que les axes partent de 0
+    ax.set_xlim3d(0, 5)
+    ax.set_ylim3d(0, 5)
+    ax.set_zlim3d(0, 5)
 
-# Données des points 
-points = ((0.8439, 2.5321, 4.2142), (3.532, 2.1024, 0.2314), (1.4, 4.6, 0.2), (1, 2, 3))
+    # Données des points 
+    points = ((0.8439, 2.5321, 4.2142), (3.532, 2.1024, 0.2314), (1.4, 4.6, 0.2), (1, 2, 3))
 
-# Ajout des deux points
-for i in points:
-    ax.scatter3D(i[0], i[1], i[2], color='red', label='point rouge')
+    # Ajout des deux points
+    for i in points:
+        ax.scatter3D(i[0], i[1], i[2], color='red', label='point rouge')
 
 
-# Ajout du segment entre les points
-for i in range(len(points) - 1):  # Parcourt jusqu'à l'avant-dernier point
-    x = [points[i][0], points[i + 1][0]]
-    y = [points[i][1], points[i + 1][1]]
-    z = [points[i][2], points[i + 1][2]]
-    ax.plot3D(x, y, z, color='green', label=f'segment{i}')
+    # Ajout du segment entre les points
+    for i in range(len(points) - 1):  # Parcourt jusqu'à l'avant-dernier point
+        x = [points[i][0], points[i + 1][0]]
+        y = [points[i][1], points[i + 1][1]]
+        z = [points[i][2], points[i + 1][2]]
+        ax.plot3D(x, y, z, color='green', label=f'segment{i}')
 
-# Ajout d'un titre et d'une légende
-ax.set_title('Les petits poingues')
-plt.show()
+    # Ajout d'un titre et d'une légende
+    ax.set_title('Les petits poingues')
+    plt.show()
 
 # Données de trajectoire
 trajectory = {
