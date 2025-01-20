d2e0b7bc3a2f74b71fecb5f35a7de0214f81c100
Théo Welker <theo.welker@heig-vd.ch>
2025-01-20 21:53:23 +0100

    Flags

--- tests_affichage_val.py
+++ tests_affichage_val.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import folium as fm
-from folium.plugins import AntPath, Fullscreen, GroupedLayerControl, MiniMap, MeasureControl, MousePosition
+from folium.plugins import AntPath, Fullscreen, MiniMap, MeasureControl, MousePosition,MarkerCluster
 import http.server
 import socketserver
 
@@ -12,34 +12,28 @@ import numpy as np
 np.set_printoptions(threshold=np.inf)
 
 fig = plt.figure() 
-if(0):
-    # Définir l'axe 3D
-    ax = plt.axes(projection='3d')
 
-    # Définir les limites pour que les axes partent de 0
-    ax.set_xlim3d(0, 5)
-    ax.set_ylim3d(0, 5)
-    ax.set_zlim3d(0, 5)
-
-    # Données des points 
-    points = ((0.8439, 2.5321, 4.2142), (3.532, 2.1024, 0.2314), (1.4, 4.6, 0.2), (1, 2, 3))
-
-    # Ajout des deux points
-    for i in points:
-        ax.scatter3D(i[0], i[1], i[2], color='red', label='point rouge')
-
-
-    # Ajout du segment entre les points
-    for i in range(len(points) - 1):  # Parcourt jusqu'à l'avant-dernier point
-        x = [points[i][0], points[i + 1][0]]
-        y = [points[i][1], points[i + 1][1]]
-        z = [points[i][2], points[i + 1][2]]
-        ax.plot3D(x, y, z, color='green', label=f'segment{i}')
-
-    # Ajout d'un titre et d'une légende
-    ax.set_title('Les petits poingues')
-    plt.show()
-
+# Définir l'axe 3D
+ax = plt.axes(projection='3d')
+# Définir les limites pour que les axes partent de 0
+ax.set_xlim3d(0, 5)
+ax.set_ylim3d(0, 5)
+ax.set_zlim3d(0, 5)
+# Données des points 
+points = ((0.8439, 2.5321, 4.2142), (3.532, 2.1024, 0.2314), (1.4, 4.6, 0.2), (1, 2, 3))
+# Ajout des deux points
+for i in points:
+    ax.scatter3D(i[0], i[1], i[2], color='red', label='point rouge')
+# Ajout du segment entre les points
+for i in range(len(points) - 1):  # Parcourt jusqu'à l'avant-dernier point
+    x = [points[i][0], points[i + 1][0]]
+    y = [points[i][1], points[i + 1][1]]
+    z = [points[i][2], points[i + 1][2]]
+    ax.plot3D(x, y, z, color='green', label=f'segment{i}')
+# Ajout d'un titre et d'une légende
+ax.set_title('Les petits poingues')
+plt.show()
+alt_max = 32000
 # Données de trajectoire
 trajectory = {
     'longitudes': [6.2000, 6.2200, 6.2400, 6.2600, 6.2800, 6.3000, 6.3079, 6.3100, 6.3150, 6.3200, 6.3250, 6.3300, 6.3350, 6.3400, 6.3450],
@@ -48,15 +42,20 @@ trajectory = {
     'times': [0, 60.0, 120.0, 180.0, 240.0, 300.0, 360.0, 420.0, 480.0, 540.0, 600.0, 660.0, 720.0, 780.0, 840.0]
 }
 
-# Création des groupes de couches pour les éléments de la carte
-fg1 = fm.FeatureGroup(name='Coordonnées')
-fg2 = fm.FeatureGroup(name='Lignes')
 
 # Initialisation de la carte centrée sur la première position
 latitudes = trajectory['latitudes']
 longitudes = trajectory['longitudes']
 altitudes = trajectory['altitudes']
 carte = fm.Map(location=[latitudes[0], longitudes[0]], zoom_start=10)
+marker_cluster = MarkerCluster().add_to(carte)
+# Trouver l'index où l'altitude dépasse alt_max
+threshold_index = next((i for i, alt in enumerate(altitudes) if alt > alt_max), len(altitudes))
+
+# Créer les coordonnées pour les deux segments
+coordinates_before = list(zip(latitudes[:threshold_index+1], longitudes[:threshold_index+1]))
+coordinates_after = list(zip(latitudes[threshold_index:], longitudes[threshold_index:]))
+
 
 # Ajout des marqueurs pour chaque point de la trajectoire
 for i, (lat, lon, alt) in enumerate(zip(latitudes, longitudes, altitudes)):
@@ -65,32 +64,57 @@ for i, (lat, lon, alt) in enumerate(zip(latitudes, longitudes, altitudes)):
     # Personnalisation des icônes selon l'altitude
     if i == 0:
         icon_url = "Miscellaneous/startflag.png"
-        custom_icon = fm.CustomIcon(icon_url, icon_size=(45, 45))
-    elif alt >= 32000:
+        custom_icon = fm.CustomIcon(icon_url, icon_size=(120,120))
+        fm.Marker(
+            location=[lat, lon],
+            popup=popup_text,
+            icon=custom_icon
+        ).add_to(carte)
+    elif alt >= alt_max:
         icon_url = "Miscellaneous/explosion.png"
-        custom_icon = fm.CustomIcon(icon_url, icon_size=(30, 30))
-    elif trajectory['altitudes'][i] == trajectory['altitudes'][-1]:
+        custom_icon = fm.CustomIcon(icon_url, icon_size=(60, 60))
+        fm.Marker(
+            location=[lat, lon],
+            popup=popup_text,
+            icon=custom_icon
+        ).add_to(carte)
+    elif alt == trajectory['altitudes'][-1]:
         icon_url = "Miscellaneous/endflag.png"
-        custom_icon = fm.CustomIcon(icon_url, icon_size=(30, 60))
+        custom_icon = fm.CustomIcon(icon_url, icon_size=(100, 100))
+        fm.Marker(
+            location=[lat, lon],
+            popup=popup_text,
+            icon=custom_icon
+        ).add_to(carte)
     else:
         icon_url = "Miscellaneous/blackcircle.png"
-        custom_icon = fm.CustomIcon(icon_url, icon_size=(10, 10))
-        
-    fm.Marker(
-        location=[lat, lon],
-        popup=popup_text,
-        icon=custom_icon
-    ).add_to(fg1)
+        custom_icon = fm.CustomIcon(icon_url, icon_size=(7, 7))
+        fm.Marker(
+            location=[lat, lon],
+            popup=popup_text,
+            icon=custom_icon
+        ).add_to(marker_cluster)
 
-# Création du trajet en utilisant AntPath
-coordinates = list(zip(latitudes, longitudes))
-fm.plugins.AntPath(
-    locations=coordinates,
-    reverse=False,
-    dash_array=[20, 30],
-    color='blue',
-    pulse_color='cyan'
-).add_to(fg2)
+threshold_index = next((i for i, alt in enumerate(altitudes) if alt > alt_max), len(altitudes))
+
+
+if coordinates_before:
+    AntPath(
+        locations=coordinates_before,
+        dash_array=[20, 30],
+        color="gray",  # Segment gris avant alt_max
+        pulse_color="white",
+        delay=500
+    ).add_to(carte)
+
+if coordinates_after:
+    AntPath(
+        locations=coordinates_after,
+        dash_array=[20, 30],
+        color="red",  # Segment rouge après alt_max
+        pulse_color="white",
+        delay=500
+    ).add_to(carte)
 
 # Ajout des plugins à la carte
 fm.plugins.Fullscreen(
@@ -104,15 +128,7 @@ MiniMap().add_to(carte)
 MeasureControl().add_to(carte)
 MousePosition().add_to(carte)
 
-# Ajout des groupes à la carte
-fg1.add_to(carte)
-fg2.add_to(carte)
 
-# Ajout du contrôle des couches pour les trajets
-GroupedLayerControl(
-    groups={'Trajets': [fg1, fg2]},
-    collapsed=False
-).add_to(carte)
 
 # Sauvegarde de la carte dans un fichier HTML
 carte.save("carte_interactive.html")
